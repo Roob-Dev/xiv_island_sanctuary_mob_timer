f2cd98778be9515b865b14e2a987620f901d0768
rubi-gallegos <rubi.g.llam@gmail.com>
2022-10-17 10:20:58 -0500

    updated table view

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,9 +1,10 @@
+import "./navbar.css";
 export default function NavBar() {
   return (
-    <div className="navbar">
-      <div className="navBarTitle">
+    <div className="navbar-brand py-2" style={{ backgroundColor: "#352f44" }}>
+      <nav className="navBarTitle">
         FFXIV Island Sanctuary Rare Mob Spawn Times
-      </div>
+      </nav>
     </div>
   );
 }
